bbc78dd2df5b25ba9e8a05f1783c82d0c0a00759
hafidhrendyanto <hafidh.rendyanto@gmail.com>
2020-05-28 15:00:07 +0700

    add main theme

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -8,10 +8,9 @@ import { HomePage } from '../home/home';
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  home = HomePage;
+  about = AboutPage;
+  contact = ContactPage;
 
   constructor() {
 
