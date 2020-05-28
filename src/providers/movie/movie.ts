a9208180b36b35a7131d4564c0d0ee42f89762d9
hafidhrendyanto <hafidh.rendyanto@gmail.com>
2020-05-28 15:00:27 +0700

    add movie providers

---
+++ src/providers/movie/movie.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MovieProvider {
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  getPopular() {
+    
+  }
+
+}
